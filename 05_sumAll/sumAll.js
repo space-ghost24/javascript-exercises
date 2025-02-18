b6c49a51cf856b75b271ba000b2bb44a4e7679a5
space-ghost24 <144731705+space-ghost24@users.noreply.github.com>
2025-02-18 13:20:17 -0600

    Complete Exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,26 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+
+    let sum = 0;
+
+    if (a < 0 || b < 0){
+        return 'ERROR';
+    }
+
+    if (!Number.isInteger(a) || !Number.isInteger(b)){
+        return 'ERROR';
+    }
+
+    if (a > b) {
+        let temp = a;
+        a = b;
+        b = temp;
+    }
+
+    for (let i = a; i <= b; i++){
+        sum += i;
+    }
+
+    return sum;
 
 };
 
