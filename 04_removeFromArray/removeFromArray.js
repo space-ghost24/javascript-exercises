91a824d3b942922a53d091548d9cb8fa4e87ff84
space-ghost24 <144731705+space-ghost24@users.noreply.github.com>
2025-02-17 18:19:07 -0600

    Shorten function again

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
 const removeFromArray = function(arr, ...args) {
-    return filtered = arr.filter(item => !args.includes(item));
+    return arr.filter(item => !args.includes(item));
 };
 
 // Do not edit below this line
