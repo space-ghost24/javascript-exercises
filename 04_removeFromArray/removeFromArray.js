2d422dcc478c30b20d8c45ca657085ddbb44589b
space-ghost24 <144731705+space-ghost24@users.noreply.github.com>
2025-02-17 18:13:34 -0600

    Complete Exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    let filtered = arr.filter(item => !args.includes(item));
+    return filtered;
 };
 
 // Do not edit below this line
