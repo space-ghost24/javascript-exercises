6bfb12e8aa96a3f1b7fd887f1648281c9bc7a326
space-ghost24 <144731705+space-ghost24@users.noreply.github.com>
2025-03-07 16:15:44 -0600

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let a = 1, b = 1
+    if (Number(n) < 0) return 'OOPS';
+    if (Number(n) === 0) return 0;
+    for (let i = 2; i < Number(n); i++) {
+        next = a + b;
+        a = b;
+        b = next;
+      }
+    return b;
 };
 
 // Do not edit below this line
