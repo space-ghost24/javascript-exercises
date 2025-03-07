6b61cc3bcb0717ac83ff63d4d8ac7bde172f2da0
space-ghost24 <144731705+space-ghost24@users.noreply.github.com>
2025-03-07 16:47:21 -0600

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldestPerson;
+    let maxAge = 0;
 
+    for(let person of people){
+        const deathYear = person.yearOfDeath || new Date().getFullYear();
+        const age = deathYear - person.yearOfBirth;
+        if (age > maxAge){
+            maxAge = age;
+            oldestPerson = person;
+        }
+    }
+    return oldestPerson;
 };
 
 // Do not edit below this line
