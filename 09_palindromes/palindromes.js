1757d40b82d61d4abfff6aac88ab0ad2d8cb5a32
space-ghost24 <144731705+space-ghost24@users.noreply.github.com>
2025-03-07 14:06:24 -0600

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const cleanedStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
 
+    const reversedStr = cleanedStr.split('').reverse().join('');
+
+    return cleanedStr === reversedStr;
 };
 
 // Do not edit below this line
