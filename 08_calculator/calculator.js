83efdb3127c464534eb12152ca31b39325661106
space-ghost24 <144731705+space-ghost24@users.noreply.github.com>
2025-03-07 13:43:25 -0600

    Complete exercise 7

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,27 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+	if (a.length === 0){
+    return 0;
+  } else {
+    return a.reduce((sum, num) => sum += num, 0);
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  if (a.length === 0){
+    return 0
+  }else{
+    return a.reduce((prod, num) => prod *= num, 1);
+  }
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let result = 1;
+  for (let i = 1; i <= a; i++) {
+    result *= i; 
+  }
+  return result;
 };
-
 // Do not edit below this line
 module.exports = {
   add,
